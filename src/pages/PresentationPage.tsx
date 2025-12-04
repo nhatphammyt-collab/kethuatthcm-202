b6ea3b4414883d16da579e54d8715748dd222161
nhatphammyt-collab <nhatphammyt@gmail.com>
2025-12-04 20:58:44 +0700

    Update PresentationPage.tsx

--- src/pages/PresentationPage.tsx
+++ src/pages/PresentationPage.tsx
@@ -102,7 +102,7 @@ export default function PresentationPage() {
         <div
           className="absolute inset-0 bg-cover bg-center"
           style={{
-            backgroundImage: 'url(/cover-kl01_1717365181.gif)',
+            backgroundImage: 'url(/sss21-jpg-3fmod-3dajperes-26amp-3bcvid-3d.png)',
             backgroundSize: 'cover',
             backgroundPosition: 'center',
             opacity: 0.85
